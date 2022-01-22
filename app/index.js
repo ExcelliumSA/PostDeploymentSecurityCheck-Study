69649b77734411544824356e7648ed9e60139570
drighetto <drighetto@excellium-services.com>
2022-01-22 11:35:58 +0100

    Start creation of the sample app

--- app/index.js
+++ app/index.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 5000
 
 express()
     .use(express.static(path.join(__dirname, 'assets')))
-    .get('/', (req, res) => res.send('Hello World2!'))
+    .get('/', (req, res) => res.send('Hello World3!'))
     .listen(port, () => console.log(`Listening on ${ port }`))
