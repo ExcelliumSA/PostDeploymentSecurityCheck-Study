1b5d7bf669bb10fa71d4a2e4078337de2399fcb3
drighetto <drighetto@excellium-services.com>
2022-01-25 14:17:13 +0100

    Continue POC impl

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000
 express()
     .use(express.static(path.join(__dirname, "assets")))
     .get("/", (req, res) => res.send("It works."))
+    .post("/axis2-admin/login", (req, res) => res.send("<h1>Welcome to Axis2 Web Admin Module !!</h1>"))
     .get("/hello", (req, res) => {
         let x = req.query.name;
         x = x.substring(5);
